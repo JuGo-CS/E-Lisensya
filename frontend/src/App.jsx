5ef317a229e2f4d0c31b6e6efb0798b6bf35dfdd
JuGo-CS <mondejarkenneth64@gmail.com>
2026-05-18 09:03:57 +0800

    fix: correct import of routes to address white screen bug

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,19 +2,18 @@ import Navbar from './components/Navbar.jsx'
 import Header from './components/Header.jsx';
 import StudentsPermits from './pages/StudentsPermits.jsx';
 import StudentRoommates from './pages/StudentRoommates.jsx';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
 
   	return (
 		<div className="min-h-screen w-full bg-slate-200 font-['Cambria',serif] overflow-hidden">
-      
+	
 			<div className="min-h-screen sm:max-w-150 mx-auto bg-white flex flex-col overflow-hidden shadow-xl">
 
 				<Header />
 				<Navbar />
 
-			<BrowserRouter>
 				<div>
 					<Routes>
 						<Route path="/" element={<StudentsPermits id={1} />} />
@@ -22,7 +21,9 @@ function App() {
 						<Route path="/profile" element={<div className="p-4">Under Construction</div>} />
 					</Routes>
 				</div>
-			</BrowserRouter>
+
+				{/* <div>
+					<StudentsPermits id={1}/>
 				</div> */}
 
 				{/* <div>
@@ -36,7 +37,7 @@ function App() {
 				<Route path="/roommates" element={<StudentRoommates id={1} />}/>
 			</Routes> */}
 
-    </div>
+		</div>
 
 
   	);
