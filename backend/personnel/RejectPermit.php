fbd0115ee46a30339af3bda6f5183ce4390e101e
Trisha Mae A. Hechenagocia <tahechenagocia@up.edu.ph>
2026-05-24 10:39:01 +0800

    feat: add reject permit endpoint

---
+++ backend/personnel/RejectPermit.php
@@ -0,0 +1,69 @@
+<?php
+// DESCRIPTION: 
+//     Handles updating permit status to rejected
+// NOTE: 
+//     Includes PersonnelMiddleware to ensure only authorized personnel members access
+
+include_once '../config/cors.php';
+
+//  restrict allowed method
+if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+    http_response_code(405); 
+    echo json_encode([
+        "success" => false, "message" => "invalid method. endpoint requires POST."
+    ]);
+    exit;
+}
+
+include_once '../config/DBConnector.php';
+include_once '../auth/PersonnelMiddleware.php'; 
+
+// get raw data
+$data = json_decode(file_get_contents("php://input"), true);
+$permit_id = isset($data['permit_id']) ? (int)$data['permit_id'] : null;
+
+// check if permit id is passed
+if (!$permit_id) {
+    echo json_encode(["success" => false, "message" => "incomplete arguments"]);
+    $conn->close();
+    exit;
+}
+
+// get permit status
+$q = $conn->prepare("SELECT status FROM permit WHERE permit_id = ?");
+$q->bind_param("i", $permit_id);
+$q->execute();
+$res = $q->get_result();
+
+// check if permit exist
+if ($res->num_rows === 0) {
+    echo json_encode(["success" => false, "message" => "permit not found"]);
+    $q->close();
+    $conn->close();
+    exit;
+}
+$row = $res->fetch_assoc();
+$permit_status = strtoupper($row['status']);
+$q->close();
+
+// action restirct to pending only
+if ($permit_status !== 'PENDING') {
+    echo json_encode(["success" => false, "message" => "only pending permits can be rejected"]);
+    $conn->close();
+    exit;
+}
+
+
+// update status and bind the person who handled it to that permit
+$upd = $conn->prepare("UPDATE permit SET status = 'REJECTED', personnel_id = ? WHERE permit_id = ?");
+$upd->bind_param("ii", $personal_id, $permit_id);
+
+if ($upd->execute() && $upd->affected_rows > 0) {
+    echo json_encode(["success" => true, "message" => "permit rejected successfully"]);
+} else {
+    echo json_encode(["success" => false, "message" => "Permit rejection unsuccessful"]);
+}
+
+$upd->close();
+$conn->close();
+?>
